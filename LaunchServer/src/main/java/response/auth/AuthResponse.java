41cf2a75e15aff1953d540a698a5565fa222c2cf
Gravit Gravit <gravit.min@ya.ru>
2018-08-11 00:52:43 +0700

    Gradle

---
+++ LaunchServer/src/main/java/response/auth/AuthResponse.java
@@ -0,0 +1,92 @@
+package launchserver.response.auth;
+
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.UUID;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+
+import launcher.helper.IOHelper;
+import launcher.helper.LogHelper;
+import launcher.helper.SecurityHelper;
+import launcher.helper.VerifyHelper;
+import launcher.serialize.HInput;
+import launcher.serialize.HOutput;
+import launchserver.LaunchServer;
+import launchserver.auth.AuthException;
+import launchserver.auth.provider.AuthProvider;
+import launchserver.auth.provider.AuthProviderResult;
+import launchserver.response.Response;
+import launchserver.response.profile.ProfileByUUIDResponse;
+
+public final class AuthResponse extends Response {
+    private final String ip;
+
+    public AuthResponse(LaunchServer server, long id, HInput input, HOutput output, String ip) {
+        super(server, id, input, output);
+        this.ip = ip;
+    }
+    public AuthResponse(LaunchServer server, long id, HInput input, HOutput output, Socket socket, String ip) {
+        super(server, id, input, output,socket);
+        this.ip = ip;
+    }
+
+    @Override
+    public void reply() throws Exception {
+        String login = input.readString(255);
+        byte[] encryptedPassword = input.readByteArray(SecurityHelper.CRYPTO_MAX_LENGTH);
+
+        // Decrypt password
+        String password;
+        try {
+            password = IOHelper.decode(SecurityHelper.newRSADecryptCipher(server.privateKey).
+                doFinal(encryptedPassword));
+        } catch (IllegalBlockSizeException | BadPaddingException ignored) {
+            requestError("Password decryption error");
+            return;
+        }
+
+        // Authenticate
+        debug("Login: '%s', Password: '%s'", login, echo(password.length()));
+        AuthProviderResult result;
+        try {
+            result = server.config.authProvider.auth(login, password, ip);
+            if (!VerifyHelper.isValidUsername(result.username)) {
+                AuthProvider.authError(String.format("Illegal result: '%s'", result.username));
+                return;
+            }
+        } catch (AuthException e) {
+            requestError(e.getMessage());
+            return;
+        } catch (Exception e) {
+            LogHelper.error(e);
+            requestError("Internal auth provider error");
+            return;
+        }
+        debug("Auth: '%s' -> '%s', '%s'", login, result.username, result.accessToken);
+
+        // Authenticate on server (and get UUID)
+        UUID uuid;
+        try {
+            uuid = server.config.authHandler.auth(result);
+        } catch (AuthException e) {
+            requestError(e.getMessage());
+            return;
+        } catch (Exception e) {
+            LogHelper.error(e);
+            requestError("Internal auth handler error");
+            return;
+        }
+        writeNoError(output);
+
+        // Write profile and UUID
+        ProfileByUUIDResponse.getProfile(server, uuid, result.username).write(output);
+        output.writeASCII(result.accessToken, -SecurityHelper.TOKEN_STRING_LENGTH);
+    }
+
+    private static String echo(int length) {
+        char[] chars = new char[length];
+        Arrays.fill(chars, '*');
+        return new String(chars);
+    }
+}
---
+++ libLauncher/src/main/java/launcher/Launcher.java
@@ -0,0 +1,84 @@
+package launcher;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.NoSuchFileException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.regex.Pattern;
+
+import launcher.helper.*;
+import launcher.serialize.HInput;
+
+public final class Launcher {
+    private static final AtomicReference<LauncherConfig> CONFIG = new AtomicReference<>();
+
+    // Version info
+    @LauncherAPI public static final String VERSION = "15.4";
+    @LauncherAPI public static final String BUILD = readBuildNumber();
+    @LauncherAPI public static final int PROTOCOL_MAGIC = 0x724724_00 + 23;
+
+    // Constants
+    @LauncherAPI public static final String RUNTIME_DIR = "runtime";
+    @LauncherAPI public static final String CONFIG_FILE = "config.bin";
+    @LauncherAPI public static final String INIT_SCRIPT_FILE = "init.js";
+
+    private static final Pattern UUID_PATTERN = Pattern.compile("-", Pattern.LITERAL);
+
+
+    @LauncherAPI
+    public static String toHash(UUID uuid) {
+        return UUID_PATTERN.matcher(uuid.toString()).replaceAll("");
+    }
+    @LauncherAPI
+    public static String jvmProperty(String name, String value) {
+        return String.format("-D%s=%s", name, value);
+    }
+    @LauncherAPI
+    public static LauncherConfig getConfig() {
+        LauncherConfig config = CONFIG.get();
+        if (config == null) {
+            try (HInput input = new HInput(IOHelper.newInput(IOHelper.getResourceURL(CONFIG_FILE)))) {
+                config = new LauncherConfig(input);
+            } catch (IOException | InvalidKeySpecException e) {
+                throw new SecurityException(e);
+            }
+            CONFIG.set(config);
+        }
+        return config;
+    }
+
+    @LauncherAPI
+    public static URL getResourceURL(String name) throws IOException {
+        LauncherConfig config = getConfig();
+        byte[] validDigest = config.runtime.get(name);
+        if (validDigest == null) { // No such resource digest
+            throw new NoSuchFileException(name);
+        }
+
+        // Resolve URL and verify digest
+        URL url = IOHelper.getResourceURL(RUNTIME_DIR + '/' + name);
+        if (!Arrays.equals(validDigest, SecurityHelper.digest(SecurityHelper.DigestAlgorithm.MD5, url))) {
+            throw new NoSuchFileException(name); // Digest mismatch
+        }
+
+        // Return verified URL
+        return url;
+    }
+
+    @LauncherAPI
+    @SuppressWarnings({ "SameReturnValue", "MethodReturnAlwaysConstant" })
+    public static String getVersion() {
+        return VERSION; // Because Java constants are known at compile-time
+    }
+
+    private static String readBuildNumber() {
+        try {
+            return IOHelper.request(IOHelper.getResourceURL("buildnumber"));
+        } catch (IOException ignored) {
+            return "dev"; // Maybe dev env?
+        }
+    }
+
+}
