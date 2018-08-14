2ba47441e656bca3e181f829ec519a609f4ff4dd
zaxar163 <zahar.vcherachny@yandex.ru>
2018-08-14 12:35:19 +0400

    Fixed deps.

--- LaunchServer/src/main/java/launchserver/plugin/bungee/LaunchServerCommandBungee.java
+++ LaunchServer/src/main/java/launchserver/plugin/bungee/LaunchServerCommandBungee.java
@@ -6,26 +6,25 @@ import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Command;
-import net.md_5.bungee.command.ConsoleCommandSender;
 
 public final class LaunchServerCommandBungee extends Command {
-    private static final BaseComponent[] NOT_CONSOLE_MESSAGE = TextComponent.fromLegacyText(ChatColor.RED + "Эту команду можно использовать только из консоли");
-    private static final BaseComponent[] NOT_INITIALIZED_MESSAGE = TextComponent.fromLegacyText(ChatColor.RED + "Лаунчсервер не был полностью загружен");
+	public static final BaseComponent[] NOT_CONSOLE_MESSAGE = TextComponent.fromLegacyText(ChatColor.RED + "Эту команду можно использовать только из консоли");
+	public static final BaseComponent[] NOT_INITIALIZED_MESSAGE = TextComponent.fromLegacyText(ChatColor.RED + "Лаунчсервер не был полностью загружен");
 
     // Instance
     public final LaunchServerPluginBungee plugin;
 
     public LaunchServerCommandBungee(LaunchServerPluginBungee plugin) {
-        super("launchserver", null, "launcher", "ls", "l");
+        super("launchserver", "launchserver.command", "launcher", "ls", "l");
         this.plugin = plugin;
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (!(sender instanceof ConsoleCommandSender)) {
+        /*if (!(sender.getClass().equals("net.md_5.bungee.command.ConsoleCommandSender"))) {
             sender.sendMessage(NOT_CONSOLE_MESSAGE);
             return;
-        }
+        }*/
 
         // Eval command
         LaunchServerPluginBridge bridge = plugin.bridge;
