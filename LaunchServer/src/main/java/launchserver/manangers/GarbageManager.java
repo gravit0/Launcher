355d2802aab39277eea4117fb1475e217944df62
Gravit Gravit <gravit.min@ya.ru>
2018-08-23 17:15:36 +0700

    Планировка задач в GarbageManager

--- LaunchServer/src/main/java/launchserver/manangers/GarbageManager.java
+++ LaunchServer/src/main/java/launchserver/manangers/GarbageManager.java
@@ -2,18 +2,42 @@ package launchserver.manangers;
 
 import launchserver.NeedGarbageCollection;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class GarbageManager {
-    private static final ArrayList<NeedGarbageCollection> NEED_GARBARE_COLLECTION = new ArrayList<>();
+    private static final Timer timer = new Timer("GarbageTimer");
+    private static final ArrayList<Entry> NEED_GARBARE_COLLECTION = new ArrayList<>();
     public static void registerNeedGC(NeedGarbageCollection gc)
     {
-        NEED_GARBARE_COLLECTION.add(gc);
+        NEED_GARBARE_COLLECTION.add(new Entry(gc,0L));
+    }
+    public static void registerNeedGC(NeedGarbageCollection gc,long time)
+    {
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                gc.garbageCollection();
+            }
+        };
+        timer.schedule(task,time);
+        NEED_GARBARE_COLLECTION.add(new Entry(gc,time));
     }
     public static void gc()
     {
-        for(NeedGarbageCollection gc : NEED_GARBARE_COLLECTION)
+        for(Entry gc : NEED_GARBARE_COLLECTION)
         {
-            gc.garbageCollection();
+            gc.invoke.garbageCollection();
+        }
+    }
+    static class Entry
+    {
+        NeedGarbageCollection invoke;
+        long timer;
+
+        public Entry(NeedGarbageCollection invoke, long timer) {
+            this.invoke = invoke;
+            this.timer = timer;
         }
     }
 }
