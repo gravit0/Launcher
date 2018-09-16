a6084b3d9f0271392352898d0259c1c113abca64
Gravit <gravit.min@ya.ru>
2018-09-16 11:26:39 +0300

    wrapper32 и тип релиза в системе версий

--- libLauncher/src/main/java/launcher/LauncherVersion.java
+++ libLauncher/src/main/java/launcher/LauncherVersion.java
@@ -6,13 +6,14 @@ import java.util.Objects;
 import launcher.helper.IOHelper;
 
 public class LauncherVersion {
-    public static int MAJOR = 4;
-    public static int MINOR = 0;
+    public static int MAJOR = 3;
+    public static int MINOR = 18;
     public static int PATCH = 0;
     public static int BUILD = readBuildNumber();
+    public static Type RELEASE = Type.EXPERIMENTAL;
 
     public static LauncherVersion getVersion() {
-        return new LauncherVersion(MAJOR,MINOR,PATCH,BUILD);
+        return new LauncherVersion(MAJOR,MINOR,PATCH,BUILD,RELEASE);
     }
     static int readBuildNumber() {
         try {
@@ -27,12 +28,14 @@ public class LauncherVersion {
     public final int patch;
 
     public final int build;
+    public final Type release;
 
     public LauncherVersion(int major, int minor, int patch) {
         this.major = major;
         this.minor = minor;
         this.patch = patch;
         build = 0;
+        release = Type.UNKNOWN;
     }
 
     public LauncherVersion(int major, int minor, int patch,int build) {
@@ -40,6 +43,14 @@ public class LauncherVersion {
         this.minor = minor;
         this.patch = patch;
         this.build = build;
+        release = Type.UNKNOWN;
+    }
+    public LauncherVersion(int major, int minor, int patch,int build,Type release) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+        this.build = build;
+        this.release = release;
     }
 
     @Override
@@ -61,12 +72,49 @@ public class LauncherVersion {
     public int hashCode() {
         return Objects.hash(major, minor, patch, build);
     }
-
+    public String getReleaseStatus()
+    {
+        String result;
+        switch (release) {
+            case LTS:
+                result="lts";
+                break;
+            case STABLE:
+                result="stable";
+                break;
+            case BETA:
+                result="beta";
+                break;
+            case ALPHA:
+                result="alpha";
+                break;
+            case DEV:
+                result="dev";
+                break;
+            case EXPERIMENTAL:
+                result="experimental";
+                break;
+            case UNKNOWN:
+                result="";
+                break;
+            default:
+                result="";
+                break;
+        }
+        return result;
+    }
     @Override
     public String toString() {
-        return        major +
-                "." + minor +
-                "." + patch +
-                "-" + build;
+        return String.format("%d.%d.%d-%d %s", major, minor, patch, build,getReleaseStatus());
+    }
+    enum Type
+    {
+        LTS,
+        STABLE,
+        BETA,
+        ALPHA,
+        DEV,
+        EXPERIMENTAL,
+        UNKNOWN
     }
 }
--- libLauncher/src/main/java/launcher/helper/LogHelper.java
+++ libLauncher/src/main/java/launcher/helper/LogHelper.java
@@ -234,8 +234,8 @@ public final class LogHelper {
         return new Ansi().bold(). // Setup
                 fgBright(Color.MAGENTA).a("sashok724's "). // sashok724's
                 fgBright(Color.CYAN).a(product). // Product
-                fgBright(Color.WHITE).a(" v").fgBright(Color.BLUE).a(Launcher.VERSION). // Version
-                fgBright(Color.WHITE).a(" (build #").fgBright(Color.RED).a(Launcher.BUILD).fgBright(Color.WHITE).a(')'). // Build#
+                fgBright(Color.WHITE).a(" v").fgBright(Color.BLUE).a(LauncherVersion.getVersion().toString()). // Version
+                fgBright(Color.WHITE).a(" (build #").fgBright(Color.RED).a(LauncherVersion.getVersion().build).fgBright(Color.WHITE).a(')'). // Build#
                 reset().toString(); // To string
     }
 
@@ -247,7 +247,7 @@ public final class LogHelper {
     }
 
     private static String formatVersion(String product) {
-        return String.format("sashok724's %s v%s (build #%s)", product, Launcher.VERSION, Launcher.BUILD);
+        return String.format("sashok724's %s v%s", product, LauncherVersion.getVersion().toString());
     }
 
     static {
