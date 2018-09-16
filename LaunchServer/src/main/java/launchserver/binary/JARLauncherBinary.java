27608373d385fc7f48968d68e621501bb6fd9584
zaxar163 <zahar.vcherachny@yandex.ru>
2018-09-16 12:37:27 +0300

    Fixed signing.

--- LaunchServer/src/main/java/launchserver/binary/JARLauncherBinary.java
+++ LaunchServer/src/main/java/launchserver/binary/JARLauncherBinary.java
@@ -65,6 +65,7 @@ public final class JARLauncherBinary extends LauncherBinary {
 			return super.visitFile(file, attrs);
 		}
 	}
+
 	private static ZipEntry newEntry(String fileName) {
 		return newZipEntry(Launcher.RUNTIME_DIR + IOHelper.CROSS_SEPARATOR + fileName);
 	}
@@ -76,11 +77,16 @@ public final class JARLauncherBinary extends LauncherBinary {
 	public final Path initScriptFile;
 
 	@LauncherAPI
+	public final Path obfJar;
+
+	@LauncherAPI
 	public JARLauncherBinary(LaunchServer server) throws IOException {
 		super(server, server.dir.resolve(server.config.binaryName + ".jar"),
-				server.dir.resolve(server.config.binaryName + "-obf.jar"));
+				server.dir.resolve(server.config.binaryName + (server.config.sign.enabled ? "-sign.jar" : "-obf.jar")));
 		runtimeDir = server.dir.resolve(Launcher.RUNTIME_DIR);
 		initScriptFile = runtimeDir.resolve(Launcher.INIT_SCRIPT_FILE);
+		obfJar = server.config.sign.enabled ? server.dir.resolve(server.config.binaryName + "-obf.jar")
+				: syncBinaryFile;
 		tryUnpackRuntime();
 	}
 
@@ -90,15 +96,13 @@ public final class JARLauncherBinary extends LauncherBinary {
 
 		// Build launcher binary
 		LogHelper.info("Building launcher binary file");
-		if (server.config.sign.enabled) signBuild();
-		else stdBuild();
+		stdBuild();
 
 		// ProGuard
 		Configuration proguard_cfg = new Configuration();
 		ConfigurationParser parser = new ConfigurationParser(
 				server.proguardConf.confStrs.toArray(new String[server.proguardConf.confStrs.size()]),
-				server.proguardConf.proguard.toFile(), 
-				System.getProperties());
+				server.proguardConf.proguard.toFile(), System.getProperties());
 		try {
 			parser.parse(proguard_cfg);
 			ProGuard proGuard = new ProGuard(proguard_cfg);
@@ -106,80 +110,20 @@ public final class JARLauncherBinary extends LauncherBinary {
 		} catch (ParseException e1) {
 			e1.printStackTrace();
 		}
+		if (server.config.sign.enabled)
+			signBuild();
 	}
 
 	private void signBuild() throws IOException {
-		try (SignerJar output = new SignerJar
-				(IOHelper.newOutput(binaryFile), 
-						SignerJar.getStore(server.config.sign.key,
-				server.config.sign.storepass, server.config.sign.algo), 
+		try (SignerJar output = new SignerJar(IOHelper.newOutput(binaryFile),
+				SignerJar.getStore(server.config.sign.key, server.config.sign.storepass, server.config.sign.algo),
 				server.config.sign.keyalias, server.config.sign.pass);
-				JAConfigurator jaConfigurator = new JAConfigurator(AutogenConfig.class)) {
-			Map<String, byte[]> outputM1 = new HashMap<>();
-			server.buildHookManager.preHook(outputM1);
-			for (Entry<String, byte[]> e : outputM1.entrySet())
-				output.addFileContents(e.getKey(), e.getValue());
-			outputM1.clear();
-			jaConfigurator.setAddress(server.config.getAddress());
-			jaConfigurator.setPort(server.config.port);
-			server.buildHookManager.registerAllClientModuleClass(jaConfigurator);
-			try (ZipInputStream input = new ZipInputStream(
-					IOHelper.newInput(IOHelper.getResourceURL("Launcher.jar")))) {
-				ZipEntry e = input.getNextEntry();
-				while (e != null) {
-					String filename = e.getName();
-					if (server.buildHookManager.isContainsBlacklist(filename)) {
-						e = input.getNextEntry();
-						continue;
-					}
-					if (filename.endsWith(".class")) {
-						CharSequence classname = filename.replace('/', '.').subSequence(0,
-								filename.length() - ".class".length());
-						byte[] bytes;
-						try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream(2048)) {
-							IOHelper.transfer(input, outputStream);
-							bytes = outputStream.toByteArray();
-						}
-						bytes = server.buildHookManager.classTransform(bytes, classname);
-						output.addFileContents(e, bytes);
-					} else
-						output.addFileContents(e, input);
-					// }
-					e = input.getNextEntry();
-				}
+				ZipInputStream input = new ZipInputStream(IOHelper.newInput(obfJar))) {
+			ZipEntry e = input.getNextEntry();
+			while (e != null) {
+				output.addFileContents(e, input);
+				e = input.getNextEntry();
 			}
-			// write additional classes
-			for (Entry<String, byte[]> ent : server.buildHookManager.getIncludeClass().entrySet()) {
-				output.addFileContents(newZipEntry(ent.getKey().replace('.', '/').concat(".class")),  server.buildHookManager.classTransform(ent.getValue(), ent.getKey()));
-			}
-			// map for runtime
-			Map<String, byte[]> runtime = new HashMap<>(256);
-			if (server.buildHookManager.buildRuntime()) {
-				// Verify has init script file
-				if (!IOHelper.isFile(initScriptFile))
-					throw new IOException(String.format("Missing init script file ('%s')", Launcher.INIT_SCRIPT_FILE));
-				// Write launcher runtime dir
-				IOHelper.walk(runtimeDir, new RuntimeDirVisitor(output.getZos(), runtime), false);
-			}
-			// Create launcher config file
-			byte[] launcherConfigBytes;
-			try (ByteArrayOutputStream configArray = IOHelper.newByteArrayOutput()) {
-				try (HOutput configOutput = new HOutput(configArray)) {
-					new LauncherConfig(server.config.getAddress(), server.config.port, server.publicKey, runtime)
-							.write(configOutput);
-				}
-				launcherConfigBytes = configArray.toByteArray();
-			}
-
-			// Write launcher config file
-			output.addFileContents(Launcher.CONFIG_FILE, launcherConfigBytes);
-			output.addFileContents(jaConfigurator.getZipEntryPath(), jaConfigurator.getBytecode());
-			server.buildHookManager.postHook(outputM1);
-			for (Entry<String, byte[]> e1 : outputM1.entrySet())
-				output.addFileContents(e1.getKey(), e1.getValue());
-			outputM1.clear();
-		} catch (CannotCompileException | NotFoundException e) {
-			LogHelper.error(e);
 		}
 	}
 
